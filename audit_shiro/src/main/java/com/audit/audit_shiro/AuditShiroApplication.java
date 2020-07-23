f83af39c7f6ff05efaa5d9ecb9df7406dd5e0d33
“youe <122717505@qq.com>
2020-07-23 16:14:24 +0800

    项目搭建

---
+++ audit_shiro/src/main/java/com/audit/audit_shiro/AuditShiroApplication.java
@@ -0,0 +1,13 @@
+package com.audit.audit_shiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditShiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditShiroApplication.class, args);
+	}
+
+}
